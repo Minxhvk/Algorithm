fd40699c46cf396e930ff87352c32726f8955517
minhyuk <minhyuk2@kakao.com>
2022-03-28 17:13:18 +0900

    더맵게

--- Programmers/Level2/더맵게.py
+++ Programmers/Level2/더맵게.py
@@ -5,8 +5,8 @@ def solution(scoville, K):
     heapq.heapify(scoville)
     cnt = 0
     while (scoville[0] < K) and len(scoville) > 1:
-        heapq.heappush(scoville, (heapq.heappop(
-            scoville)+(heapq.heappop(scoville)*2)))
+        heapq.heappush(scoville,
+                       (heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)))
         cnt += 1
     if scoville[0] < K:
         return -1
