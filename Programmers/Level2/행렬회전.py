0cc6f3586d3adc965089c2417c18cfc96209aba6
minhyuk <minhyuk2@kakao.com>
2022-03-30 12:11:32 +0900

    행렬회전

---
+++ Programmers/Level2/행렬회전.py
@@ -0,0 +1,41 @@
+def solution(rows, columns, queries):
+    answer = []
+    nums = [[0 for col in range(columns)] for row in range(rows)]
+    num = 1
+    for i in range(rows):
+        for j in range(columns):
+            nums[i][j] = num
+            num += 1
+
+    for y1, x1, y2, x2 in queries:
+        y1 -= 1
+        x1 -= 1
+        y2 -= 1
+        x2 -= 1
+        tmp = nums[y1][x1]
+        min_value = tmp
+
+        for i in range(y1, y2):
+            value = nums[i+1][x1]
+            nums[i][x1] = value
+            min_value = min(min_value, value)
+
+        for i in range(x1, x2):
+            value = nums[y2][i+1]
+            nums[y2][i] = value
+            min_value = min(min_value, value)
+
+        for i in range(y2, y1, -1):
+            value = nums[i-1][x2]
+            nums[i][x2] = value
+            min_value = min(min_value, value)
+
+        for i in range(x2, x1, -1):
+            value = nums[y1][i-1]
+            nums[y1][i] = value
+            min_value = min(min_value, value)
+
+        nums[y1][x1 + 1] = tmp
+        answer.append(min_value)
+
+    return answer
