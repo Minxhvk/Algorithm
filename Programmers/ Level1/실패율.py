b7d8fdd7a23e11a77d172c0300aecd064f207f57
minhyuk <minhyuk2@kakao.com>
2022-04-01 22:16:47 +0900

    실패율

--- Programmers/ Level1/실패율.py
+++ Programmers/ Level1/실패율.py
@@ -1,28 +1,16 @@
 import collections
 
-# 정확성 96.3
+# 다른 사람 풀이
 
 
 def solution(N, stages):
-    answer = []
-    rate_dict = collections.defaultdict(int)
-    all_challenge = 0
-    not_complete = 0
-
-    for idx, val in enumerate(sorted(stages)):
-        if val > N:
-            val = N
-        if rate_dict[val] != 0:
-            continue
-        all_challenge = len(stages) - idx
-        not_complete = stages.count(val)
-        rate = not_complete / all_challenge
-        rate_dict[val] = rate
-
-    for i in range(1, N+1):
-        if rate_dict[i] == 0:
-            rate_dict[i] = 0
-    for i, j in sorted(rate_dict.items(), key=lambda item: item[1], reverse=True):
-        answer.append(i)
-
-    return answer
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
+        else:
+            result[stage] = 0
+    return sorted(result, key=lambda x: result[x], reverse=True)
