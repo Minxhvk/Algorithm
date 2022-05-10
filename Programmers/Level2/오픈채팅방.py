e1fd3b299323be99572b41ef9ff5f0b9ce22bd42
minhyuk <minhyuk2@kakao.com>
2022-05-10 20:30:57 +0900

    Programmers 오픈채팅방

--- Programmers/Level2/오픈채팅방.py
+++ Programmers/Level2/오픈채팅방.py
@@ -1,5 +1,35 @@
 from collections import defaultdict
 
+# 처음 풀이
+
+
+def solution(record):
+    name_dict = defaultdict(int)
+    answer = []
+
+    for i in record[::-1]:
+        record_str = i.split()
+        if record_str[0] == "Leave":
+            continue
+        user_id = record_str[1]
+        user_name = record_str[2]
+        if name_dict[user_id] == 0:
+            name_dict[user_id] = user_name
+
+    for i in record:
+        record_str = i.split()
+        user_do = record_str[0]
+        user_id = record_str[1]
+
+        if user_do == "Enter":
+            answer.append(name_dict[user_id] + "님이 들어왔습니다.")
+        elif user_do == "Leave":
+            answer.append(name_dict[user_id] + "님이 나갔습니다.")
+
+    return answer
+
+# 수정 풀이
+
 
 def solution(record):
     name_dict = defaultdict(int)
