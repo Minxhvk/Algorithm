d4acad1f5cfa127487fb992a9aa4eb738c49e19d
minhyuk <minhyuk2@kakao.com>
2022-06-26 17:58:43 +0900

    Programmers 섬 연결하기

---
+++ Programmers/Level2/섬 연결하기.py
@@ -0,0 +1,38 @@
+def find(n):
+    if (n == node_list[n]):
+        return n
+    else:
+        return find(node_list[n])
+
+
+def unionParent(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        node_list[b] = a
+    else:
+        node_list[a] = b
+
+
+def compareParent(a, b):
+    # 최상위 노드 검색, 같을 경우 사이클이므로 수행 X
+    a = find(a)
+    b = find(b)
+    if a == b:
+        return True
+    else:
+        return False
+
+
+def solution(n, costs):
+    global node_list
+    answer = 0
+    node_list = [i for i in range(n)]
+    costs.sort(key=lambda x: x[2])
+
+    for parent, child, cost in costs:
+        if not compareParent(parent, child):
+            answer += cost
+            unionParent(parent, child)
+
+    return answer
