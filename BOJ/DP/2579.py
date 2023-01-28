7c5e9dfb7a9e82d5bf47738c2ab66b2d7ea38652
Minxhvk <minxhvk@gmail.com>
2023-01-28 21:36:16 +0900

    BOJ 2579

--- BOJ/DP/2579.py
+++ BOJ/DP/2579.py
@@ -11,14 +11,12 @@ arr = [[0 for _ in range(3)] for _ in range(n+1)]
 
 for _ in range(n):
     board.append(get())
-
-arr[1][1] = board[0]
-try:
-    arr[2][1] = board[1]
-    arr[2][2] = board[0] + board[1]
-except IndexError:
+if n == 1:
     print(board[0])
     exit()
+arr[1][1] = board[0]
+arr[2][1] = board[1]
+arr[2][2] = board[0] + board[1]
 
 for i in range(3, n+1):
     arr[i][1] = max(arr[i-2][1], arr[i-2][2]) + board[i-1]
