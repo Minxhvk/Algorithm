ba93df15aea4f730e0ca32a34508de9cb2c3a97b
Minxhvk <minxhvk@gmail.com>
2023-01-29 17:51:32 +0900

    BOJ 12852

--- BOJ/DP/12852.py
+++ BOJ/DP/12852.py
@@ -9,8 +9,8 @@ arr[1][0] = 0
 arr[1][1] = [1]
 
 for i in range(2, n+1):
-    arr[i][0] = arr[i-1][0] + 1
-    arr[i][1] = arr[i-1][1] + [i]
+    arr[i][0] = arr[i-1][0] + 1  # 최솟값 계산
+    arr[i][1] = arr[i-1][1] + [i]  # 경로 추적
 
     if i % 3 == 0 and arr[i//3][0] + 1 < arr[i][0]:
         arr[i][0] = arr[i//3][0] + 1
